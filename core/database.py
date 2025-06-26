d06cd590ee6f29e985d9ec8755e61b7ddcf52421
vedhanayagan9626 <vedhanayagant2000@gmail.com>
2025-06-26 11:27:40 +0530

    Initialized .env variable and create engine change for mssql

--- core/database.py
+++ core/database.py
@@ -8,7 +8,8 @@ load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 if DATABASE_URL is None:
 	raise ValueError("DATABASE_URL environment variable is not set")
-engine = create_engine(DATABASE_URL, echo=True, fast_executemany=True)
+# engine = create_engine(DATABASE_URL, echo=True, fast_executemany=True)
+engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
 
